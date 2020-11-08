b0bedb1f13075b481a28ed3d35317f8a4ef398ad
igroykt <artofmotion.igroman@gmail.com>
2020-11-08 21:15:11 +0900

    generate_cert_for update; generate_config_for update

--- caapi.py
+++ caapi.py
@@ -7,18 +7,20 @@ class CAApi:
     server = ""
     user = ""
     remote_tmp = ""
-    ca_name
+    local_storage = ""
+    ca_name = ""
     cert_template = ""
 
-    def __init__(self, server, user, remote_tmp, ca_name, cert_template):
+    def __init__(self, server, user, remote_tmp, local_storage, ca_name, cert_template):
         self.server = server
         self.user = user
         self.remote_tmp = remote_tmp
+        self.local_storage = local_storage
         self.ca_name = ca_name
         self.cert_template = cert_template
 
     def call(self, command):
-	    process = subprocess.Popen(command,stdout = subprocess.PIPE,stderr = subprocess.PIPE,shell = True, universal_newlines = True)
+	    process = subprocess.Popen(command,stdout = subprocess.PIPE,stderr = subprocess.PIPE, shell = True, universal_newlines = True, errors = "ignore")
 	    std_out,std_err = process.communicate()
 	    return process.returncode, std_out, std_err
 
@@ -43,56 +45,68 @@ class CAApi:
         except Exception as e:
             return e
 
-    def get_domain_netbios(self):
+    '''def get_domain_netbios(self):
         try:
             out = self.ssh("echo %USERDOMAIN%")
             return out
         except Exception as e:
             return e
 
-    def generate_config(self, requester):
-        short_domain = self.get_domain_netbios()
+    def get_domain_hostname(self):
+        try:
+            out = self.ssh("hostname")
+            return out
+        except Exception as e:
+            return e'''
+
+
+    def generate_config_for(self, user_fullname, user_dn, user_mail, user_domain):
+        dn = user_dn.split("@")
+        requester = dn[0]
         config = configparser.ConfigParser()
         config['Version'] = {
             'Signature': '"$Windows NT$"'
         }
         config['NewRequest'] = {
-            'Subject': f'"CN={requester}"',
-            'KeySpec': '1',
+            'Subject': f'"CN={user_fullname}"',
             'KeyLength': '2048',
-            'Exportable': 'true',
-            'MachineKeySet': 'true',
-            'SMIME': 'false',
-            'PrivateKeyArchive': 'false',
-            'UserProtected': 'false',
-            'UseExistingKeySet': 'false',
-            'ProviderName': '"Microsoft RSA SChannel Cryptographic Provider"',
-            'ProviderType': '12',
+            'KeySpec': '1',
+            'KeyUsage': '0xa0',
+            'ProviderName' : '"Microsoft RSA SChannel Cryptographic Provider"',
+            'ProviderType': '1',
             'RequestType': 'CMC',
-            'RequesterName': f'"{short_domain}\{requester}"',
-            'KeyUsage': '0xa0'
+            'RequesterName': f'"{user_domain}\{requester}"',
         }
         config['RequestAttributes'] = {
-            'CertificateTemplate': self.cert_template
+            'CertificateTemplate': f'{self.cert_template}'
         }
-        config['EnhancedKeyUsageExtension'] = {
-            # server authentication OID
-            'OID': '1.3.6.1.5.5.7.3.1'
+        config['Extensions'] = {
+            '2.5.29.17': '"{text}"',
+            '_continue_': f'"email={user_mail}&"'
         }
         try:
             with open(f'/tmp/{requester}.ini', 'w') as configfile:
                 config.write(configfile)
-                return True
+            self.call(f"sed -i '$ d' /tmp/{requester}.ini")
+            self.call(f"echo '_continue_ = \"upn={user_dn}&\"' >> /tmp/{requester}.ini")
+            return True
         except Exception as e:
             return e
 
-    def request_cert_for(self, requester):
+    def generate_cert_for(self, user_dn, cep_cert, cert_pass):
         try:
-            self.call(f"openssl req -sha256 -key /tmp/{requester}.key -new -out /tmp/{requester}.csr -config /tmp/{requester}.ini")
-            self.scp_put(f"/tmp/{requester}.csr, {self.remote_tmp}")
-            self.ssh(f"certreq -submit -config {self.ca_name} -attrib 'CertificateTemplate:{self.cert_template}' {self.remote_tmp}\\{requester}.csr {self.remote_tmp}\\{requester}.cer")
-            self.scp_get(f"{self.remote_tmp}\\{requester}.cer /tmp/")
-            self.ssh(f"del /F /Q {self.remote_tmp}\\{requester}.ini {self.remote_tmp}\\{requester}.csr {self.remote_tmp}\\{requester}.cer")
+            dn = user_dn.split("@")
+            requester = dn[0]
+            self.scp_put(f"/tmp/{requester}.ini, {self.remote_tmp}")
+            self.ssh(f"certreq -f -new -config {hostname}\{self.ca_name} {self.remote_tmp}\\{requester}.ini {self.remote_tmp}\\{requester}.req")
+            self.ssh(f"certreq -f -q -config {hostname}\{self.ca_name} -sign -cert {cep_cert} {self.remote_tmp}\\{requester}.req {self.remote_tmp}\\{requester}_signed.req")
+            self.ssh(f"certreq -submit -config {hostname}\{self.ca_name} -attrib 'CertificateTemplate: {self.cert_template}' {self.remote_tmp}\\{requester}_signed.req {self.remote_tmp}\\{requester}.cer")
+            self.ssh(f"certutil -addstore -f MY {self.remote_tmp}\\{requester}.cer")
+            self.ssh(f"certutil -repairstore MY {user_dn}")
+            self.ssh(f"certutil -p {cert_pass} -exportPFX {user_dn} {self.remote_tmp}\\{requester}.pfx")
+            self.ssh(f"certutil -privatekey –delstore MY {user_dn}")
+            self.scp_get(f"{self.remote_tmp}\\{requester}.pfx, {self.local_storage}")
+            self.ssh(f"del /F /Q {self.remote_tmp}\\{requester}.cer {self.remote_tmp}\\{requester}.ini {self.remote_tmp}\\{requester}.pfx {self.remote_tmp}\\{requester}.req {self.remote_tmp}\\{requester}.rsp {self.remote_tmp}\\{requester}_signed.req")
             return True
         except Exception as e:
             return e
